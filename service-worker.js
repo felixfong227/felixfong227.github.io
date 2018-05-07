3e52722d39e25975a2c55bb9f6616a8ec78436a1
Felix Fong <moongod101@hotmail.com>
2018-05-07 16:45:47 +0800

    Add service worker

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js')
+
+const preCacheList = [
+    '/',
+    '/index.html',
+    'https://fonts.googleapis.com/css?family=Pangolin',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.googleapis.com/css?family=Skranji',
+    '/bundle.js',
+    'https://avatars0.githubusercontent.com/u/13918481?v=4',
+    'https://i.imgur.com/NiMys9W.png',
+    'https://i.imgur.com/lvj6bqd.png',
+]
+
+if(workbox){
+    workbox.precaching.precacheAndRoute(preCacheList)
+
+    workbox.routing.registerRoute(
+        new RegExp(/.*/),
+        workbox.strategies.staleWhileRevalidate(),
+    )
+}
