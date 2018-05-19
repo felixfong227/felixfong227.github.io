f53e317a1d444edefa52b6f58d89c9a653513e9a
Felix Fong <moongod101@hotmail.com>
2018-05-19 23:08:04 +0800

    Remove the bundle and index.html from the precache list

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,9 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js')
 
 const preCacheList = [
-    '/index.html',
     'https://fonts.googleapis.com/css?family=Pangolin',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
     'https://fonts.googleapis.com/css?family=Skranji',
-    '/bundle.js',
     'https://avatars0.githubusercontent.com/u/13918481?v=4',
     'https://i.imgur.com/NiMys9W.png',
     'https://i.imgur.com/lvj6bqd.png',
