9d2e9e99064e73098e58345f00444bfb2accf65e
Felix Fong <moongod101@hotmail.com>
2018-10-28 16:34:54 +0800

    Create a new util function for checking if WebP image format are supported

---
+++ src/Utils/CheckWebpSupport.js
@@ -0,0 +1,6 @@
+export default async function checkWebpSupport() {
+    if (!self.createImageBitmap) return false;
+    const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+    const blob = await fetch(webpData).then(r => r.blob());
+    return createImageBitmap(blob).then(() => true, () => false);
+}
